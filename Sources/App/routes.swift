619d077915e16fe8d4765e70e76e3041eb4ccd91
saddam satouyev <48884148+vayset@users.noreply.github.com>
2021-09-20 16:37:21 +0200

    [master] Add hello saddam page

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -9,6 +9,11 @@ func routes(_ app: Application) throws {
     app.get("hello") { req -> String in
         return "Hello, world!"
     }
+    
+    app.get("hello-saddam") { req in
+        return req.view.render("saddam", ["title": "Hello Saddam!"])
+    }
 
     try app.register(collection: TodoController())
 }
+
